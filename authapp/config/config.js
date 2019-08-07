e4d53b54dda69cfe8084de7db18e863487f33688
Rizal Iman <Rizaliman@inovasisolusi.com>
2019-08-07 13:34:08 +0700

    first-commit

--- authapp/config/config.js
+++ authapp/config/config.js
@@ -1,4 +1,14 @@
 module.exports = {
-    JWT_SECRET : "sskntlmns3797"
+    JWT_SECRET : "sskntlmns3797",
+    oauth: {
+        google: {
+            clientID : "640481382013-9mmt4q5v3i6j0d0euognelr3hl581tps.apps.googleusercontent.com",
+            clientSecret : "Su0VJoljxG5xIlqrV_DQoZ8N"
+        },
+        facebook: {
+            clientID: '',
+            clientSecret: ''
+        }
+    }
 
 }
--- authapp/passport.js
+++ authapp/passport.js
@@ -1,15 +1,17 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
-const { JWT_SECRET } = require('./config/config');
+// const { JWT_SECRET } = require('./config/config');
 const LocalStrategy = require('passport-local');
 const User = require('./models/users');
 const GooglePlusTokenStrategy = require('passport-google-plus-token');
+const config =require('./config/config');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 //JSON WEB TOKENS STRATEGY
 passport.use(new JwtStrategy({ 
     jwtFromRequest : ExtractJwt.fromHeader('authorization'),
-    secretOrKey : JWT_SECRET
+    secretOrKey : config.JWT_SECRET
 },
  async (payload, done) => {
     //fing user in token
@@ -29,12 +31,10 @@ passport.use(new JwtStrategy({
 passport.use(new LocalStrategy({
     usernameField : 'email',
 }, async (email, password, done) =>{
-    console.log("iam here");
-    User.findOne({email}, (err, user) =>{
+    User.findOne({"local.email" : email}, (err, user) =>{
         if(!user){
             return done(null, false);
         }
-        console.log("iamstuck here");
         user.isValidPassword(password).then((res)=>{
             if(!res){
                 done(null, false);
@@ -46,8 +46,8 @@ passport.use(new LocalStrategy({
 
 // GOOGLE OAUTH STRATEGY
 passport.use('googleToken', new GooglePlusTokenStrategy({
-    clientID : "640481382013-9mmt4q5v3i6j0d0euognelr3hl581tps.apps.googleusercontent.com",
-    clientSecret : "Su0VJoljxG5xIlqrV_DQoZ8N"
+    clientID : config.oauth.google.clientID,
+    clientSecret : config.oauth.google.clientSecret
 }, async (accessToken, refreshToken, profile, done) => {
     // console.log('accessToken', accessToken);
     // console.log('refreshToken', refreshToken);
@@ -73,3 +73,33 @@ passport.use('googleToken', new GooglePlusTokenStrategy({
         console.log(err);
     })
 }))
+
+
+// passport.use('facebookToken', new GooglePlusTokenStrategy({
+//     clientID : config.oauth.facebook.clientID,
+//     clientSecret : config.oauth.facebook.clientSecret
+// }, async (accessToken, refreshToken, profile, done) => {
+//     // console.log('accessToken', accessToken);
+//     // console.log('refreshToken', refreshToken);
+//     // console.log('profile', profile);
+
+//     User.findOne({"google.id" : profile.id}).then( (res) => {
+//         if(res){
+//             return done(null, res);
+//         }
+//         const user = new User({
+//             method : 'google',
+//             google:{
+//                 id : profile.id,
+//                 email : profile.emails[0].value,
+//                 name : profile.displayName
+//             }
+//         });
+//         user.save().then( (res) => {
+//             done(null, user);
+//         });
+//     })
+//     .catch( (err) => {
+//         console.log(err);
+//     })
+// }))
